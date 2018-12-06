3d2d20fdff42f748299f7f8b3fa09a3e08dbad3c
Victor Molina <victor80@gmail.com>
2018-12-07 00:46:39 +0100

    read assets from json file

--- src/main/java/com/tankmania/game/config/Configuration.java
+++ src/main/java/com/tankmania/game/config/Configuration.java
@@ -1,13 +1,16 @@
 package com.tankmania.game.config;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.tankmania.game.model.Asset;
 import com.tankmania.game.store.AssetStore;
 
+import java.io.InputStreamReader;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Configuration extends AbstractModule {
 
@@ -16,14 +19,15 @@ public class Configuration extends AbstractModule {
     @Override
     protected void configure() {
         super.configure();
-
-        List<Asset> initialTestAssets = new CopyOnWriteArrayList<>();
-        initialTestAssets.add(new Asset("MAIN_BACKGROUND", "0.0.1", "https://www.domain.es/main/background/tankmania.png"));
-
-        bind(AssetStore.class).toInstance(new AssetStore(initialTestAssets));
+        bind(AssetStore.class).toInstance(new AssetStore(readAssets()));
     }
 
     public static <T> T getInstance(Class<T> clazz) {
         return injector.getInstance(clazz);
+    }
+
+    private List<Asset> readAssets() {
+        JsonReader reader = new JsonReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("assets.json")));
+        return new Gson().fromJson(reader, new TypeToken<List<Asset>>(){}.getType());
     }
 }
