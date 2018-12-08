a3c63983a7c3d1c243df612bd84a95d5f7c12832
Victor Molina <victor80@gmail.com>
2018-12-08 02:27:23 +0100

    Google Drive

--- common/src/main/java/com/tankmania/common/store/AssetStore.java
+++ common/src/main/java/com/tankmania/common/store/AssetStore.java
@@ -1,39 +1,81 @@
 package com.tankmania.common.store;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import com.tankmania.common.model.Asset;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class AssetStore {
 
-    private final List<Asset> assets;
+    private static final String APPLICATION_NAME = "tankmania";
+    private static final String USER_ID = "tankmania.seguiwars@gmail.com";
+    private static final String FILE_ID = "10lWfnHt9Ie5ONyJjmiTljAuQwf86xOIpU7-wkJKWqtE";
+    private static final String CREDENTIALS_FILE = "/client_id.json";
+
+    private final Drive driveService;
 
     public AssetStore() {
-        this.assets = readAssets();
+        this.driveService = createDriveService();
     }
 
     public List<Asset> getAssets() {
-        return assets;
-    }
-
-    public void setAssets() {
-
-    }
-
-    private List<Asset> readAssets() {
         try {
-            JsonReader reader = new JsonReader(new InputStreamReader(new FileInputStream("c://assets.json")));
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            driveService.files().export(FILE_ID, "text/plain").executeMediaAndDownloadTo(outputStream);
+            JsonReader reader = new JsonReader(new InputStreamReader(new ByteArrayInputStream(outputStream.toByteArray()), "UTF-8"));
             return new Gson().fromJson(reader, new TypeToken<List<Asset>>(){}.getType());
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return new ArrayList<Asset>();
         }
+    }
+
+    public void setAssets() {
+        // TODO
+    }
+
+    private Drive createDriveService() {
+        try {
+            final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            return new Drive.Builder(HTTP_TRANSPORT, JacksonFactory.getDefaultInstance(), getCredentials(HTTP_TRANSPORT))
+                    .setApplicationName(APPLICATION_NAME)
+                    .build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        InputStream in = AssetStore.class.getResourceAsStream(CREDENTIALS_FILE);
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JacksonFactory.getDefaultInstance(), new InputStreamReader(in));
+
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JacksonFactory.getDefaultInstance(), clientSecrets, Collections.singletonList(DriveScopes.DRIVE))
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File("tokens")))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize(USER_ID);
     }
 }
